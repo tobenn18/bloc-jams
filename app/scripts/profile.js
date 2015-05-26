13f4378494d4e72117949cb8efb3787373a5d459
Ben Glumack <bglumack@hotmail.com>
2015-05-26 17:19:51 +0000

    Converted landing page to angular

--- app/scripts/profile.js
+++ app/scripts/profile.js
@@ -1,6 +1,6 @@
 // holds the name of our tab button container for selection later in the function
 var tabsContainer = ".user-profile-tabs-container"
-var selectTabHandler = function(event){
+var selectTabHandler = function (event){
     $tab = $(this);
     $(tabsContainer + " li").removeClass('active');
     $tab.parent().addClass('active');
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -267,10 +267,31 @@ if (document.URL.match(/\/album.html/)) {
 });
 
 ;require.register("scripts/app", function(exports, require, module) {
-require('./landing');
-require('./collection');
-require('./album');
-require('./profile');
+//require('./landing');
+//require('./collection');
+//require('./album');
+//require('./profile');
+
+angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+  $scope.subText = "Turn the music up!";
+
+  $scope.subTextClicked = function(){
+    $scope.subText += '!';
+  };
+
+  $scope.albumURLs = [
+     '/images/album-placeholders/album-1.jpg',
+     '/images/album-placeholders/album-2.jpg',
+     '/images/album-placeholders/album-3.jpg',
+     '/images/album-placeholders/album-4.jpg',
+     '/images/album-placeholders/album-5.jpg',
+     '/images/album-placeholders/album-6.jpg',
+     '/images/album-placeholders/album-7.jpg',
+     '/images/album-placeholders/album-8.jpg',
+     '/images/album-placeholders/album-9.jpg',
+  ];
+}]);
+
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
@@ -385,7 +406,7 @@ if (document.URL.match(/\/collection.html/)) {
 ;require.register("scripts/profile", function(exports, require, module) {
 // holds the name of our tab button container for selection later in the function
 var tabsContainer = ".user-profile-tabs-container"
-var selectTabHandler = function(event){
+var selectTabHandler = function (event){
     $tab = $(this);
     $(tabsContainer + " li").removeClass('active');
     $tab.parent().addClass('active');
