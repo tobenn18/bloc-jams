b5c5664d2590cc3a006c7ee2718d1773da68ce82
Ben Glumack <bglumack@hotmail.com>
2015-05-27 18:26:30 +0000

    Converted Bloc Jams to a single page application

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -271,15 +271,26 @@ if (document.URL.match(/\/album.html/)) {
 //require('./collection');
 //require('./album');
 //require('./profile');
+blocJams = angular.module('BlocJams', ['ui.router']);
+blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+   $locationProvider.html5Mode(true);
+ 
+   $stateProvider.state('landing', {
+     url: '/',
+     controller: 'Landing.controller',
+     templateUrl: '/templates/landing.html'
+   });
+ }]);
+ 
+ // This is a cleaner way to call the controller than crowding it on the module definition.
+ blocJams.controller('Landing.controller', ['$scope', function($scope) {
+   $scope.subText = "Turn the music up!";
+   
+   $scope.subTextClicked = function() {
+     $scope.subText += '!';
+   };      
 
-angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
-  $scope.subText = "Turn the music up!";
-
-  $scope.subTextClicked = function(){
-    $scope.subText += '!';
-  };
-
-  $scope.albumURLs = [
+   $scope.albumURLs = [
      '/images/album-placeholders/album-1.jpg',
      '/images/album-placeholders/album-2.jpg',
      '/images/album-placeholders/album-3.jpg',
@@ -289,8 +300,18 @@ angular.module('BlocJams', []).controller('Landing.controller', ['$scope', funct
      '/images/album-placeholders/album-7.jpg',
      '/images/album-placeholders/album-8.jpg',
      '/images/album-placeholders/album-9.jpg',
-  ];
+   ];
+ 
+ $scope.shuffle = function(o){ //v1.0
+        for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+    };
+ $scope.shuffleClicked = function(){
+    $scope.shuffle($scope.albumURLs);
+ };
+
 }]);
+
 
 });
 
